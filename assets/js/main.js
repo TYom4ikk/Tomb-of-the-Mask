f9f9c3e5143a59a54597788a01d929e3e23852aa
Artyom <Zhakubekov20032006@gmail.com>
2024-10-29 17:26:43 +0500

    Создание карты и чето там ещё

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,12 +24,14 @@ let play_but = document.querySelector("article .play");
 let canvas = document.querySelector("#gameZone");
 let context = canvas.getContext("2d");
 
-let size = 60;
+let size = 30;
 let player = {
-    position: {x:0, y:hub.clientHeight },
-    speed: size,
+    position: {x:0, y:0},
+    speed: 30,
     img: new Image(),
-    direction:"";
+    direction:"",
+    timeSpeed: 5,
+    coin_count: 0
 }
 
 player.img.src = "assets/img/player/player.png";
@@ -46,26 +48,96 @@ play_but.addEventListener("click",function(){
 
 function render(){
     context.clearRect(0,0,canvas.width, canvas.height);
-    context.drawImage(player.img,
+    context.drawImage(
+        player.img,
         player.position.x,
         player.position.y,
         size,
         size
     );
     for (let item = 0; item < map.length; item++) {
-        
-        context.strokeRect(map[item].x, map[item].y,size,size);    
+        if(map[item].type != null){
+            context.drawImage(
+                map[item].type.img,
+                map[item].x,
+                map[item].y,
+                size,
+                size
+            );  
+        } 
     }
-    
+    renderCeil(map);
     requestAnimationFrame(render);
 }
 
 let map = [];
 function genMap(){
     for (let x = 0; x < canvas.width; x+=size) {
-        for (let y = 0 + hub.clientHeight; y < canvas.height; y+=size) {
-            map.push({x:x,y:y})
+        for (let y = 0; y < canvas.height; y+=size) {
+            map.push({
+                x:x,
+                y:y,
+                type: null               
+            })
         }       
+    }
+    setMap();
+}
+
+function collision(){
+    let result = map.find(item =>
+        item.x <= player.position.x + size
+        &&
+        item.x + size >= player.position.x
+        &&
+        item.y <= player.position.y + size
+        &&
+        item.y + size >= player.position.y
+    );
+    console.log(result.type);
+    if(result.type != null){
+        switch(result.type.name){
+            case "coin":
+                player.coin_count++;
+                result.type = null;
+                break;
+            case "wall":
+                let old_direction = player.direction;
+                player.direction = "";
+                clearInterval(player.move);
+                player.move = null;
+                switch(old_direction){
+                    case "up":
+                        player.position.y = result.y + size;
+                        break;
+                    case "down":
+                        player.position.y = result.y - size;
+                        break;
+                    case "left":
+                        player.position.x = result.x+size;
+                        break;
+                    case "right":
+                        player.position.x = result.x - size;
+                        break;
+                }
+                break;
+            }
+        }
+    }
+
+function renderCeil(map){
+    for (let item = 0; item < map.length; item++){
+        context.strokeRect(
+            map[item].x,
+            map[item].y,
+            size,
+            size
+        );
+        context.fillText(
+            item,
+            map[item].x + size/2,
+            map[item].y + size/2
+        );
     }
 }
 
@@ -73,27 +145,78 @@ document.addEventListener("keydown", function(event){
     console.log(event.keyCode);
     switch(event.keyCode){
         case 87: //w
-            if(player.position.y > 0 + hub.clientHeight){
-                player.position.y -= player.speed;
-            }
+        if(player.move == null){
+            player.direction = "up";
+            player.move = setInterval(move, player.timeSpeed);
+        }
         break;
         case 83: //s
-            if(player.position.y+size < canvas.height){
-                player.position.y += player.speed;
-            }
+
+        if(player.move == null){
+            player.direction = "down";
+            player.move = setInterval(move, player.timeSpeed);
+        }
         break;
         case 65: //a
-            if (player.position.x > 0 ){
-                player.position.x -= player.speed;
-            }
+        if(player.move == null){
+            player.direction = "left";
+            player.move = setInterval(move, player.timeSpeed);
+        }
         break;
         case 68: //d
-            if(player.position.x + size < canvas.width){
-                player.position.x += player.speed;
-            }
+        if(player.move == null){
+            player.direction = "right";
+            player.move = setInterval(move, player.timeSpeed);
+        }
         break;
     }
 });
+
+function move(){
+    switch(player.direction){
+        case "up":
+            if(player.position.y>0){
+                player.position.y -= player.speed;
+            }
+            else{
+                player.direction = "";
+                clearInterval(player.move);
+                player.move = null;
+            }
+            break;
+        case "down":
+            if(player.position.y+size<canvas.height){
+                player.position.y += player.speed;
+            }
+            else{
+                player.direction = "";
+                clearInterval(player.move);
+                player.move = null;
+            }
+            break;
+        case "left":
+            if(player.position.x>0){
+                player.position.x -= player.speed;
+            }
+            else{                    
+                player.direction = "";
+                clearInterval(player.move);
+                player.move = null;
+            }
+            break;
+        case "right":
+            if(player.position.x + size<canvas.width){
+                player.position.x += player.speed;
+            }
+            else{
+                player.direction = "";
+                clearInterval(player.move);
+                player.move = null;
+            }
+            break;
+    }
+    collision();
+}
 
 function hideElement(element){
     element.style.display = "none";
---
+++ assets/js/map.js
@@ -0,0 +1,94 @@
+let coin_img = new Image();
+coin_img.src = "assets/img/icons/Icon=coin.png";
+
+let wall_bottom1_img = new Image();
+wall_bottom1_img.src = "assets/img/map/Numer Box=B1.png";
+
+let wall_b2_img = new Image();
+wall_b2_img.src = "assets/img/Numer Box=B2.png";
+
+let wall_b3_img = new Image();
+wall_b3_img.src = "assets/img/Numer Box=B3.png";
+
+let wall_b4_img = new Image();
+wall_b4_img.src = "assets/img/Numer Box=B4.png";
+
+let wall_b5_img = new Image();
+wall_b5_img.src = "assets/img/Numer Box=B5.png";
+
+let wall_b6_img = new Image();
+wall_b6_img.src = "assets/img/Numer Box=B6.png";
+
+let wall_b7_img = new Image();
+wall_b7_img.src = "assets/img/Numer Box=B7.png";
+
+let wall_b8_img = new Image();
+wall_b8_img.src = "assets/img/Numer Box=B8.png";
+
+let wall_b9_img = new Image();
+wall_b9_img.src = "assets/img/Numer Box=B9.png";
+
+let wall_b10_img = new Image();
+wall_b10_img.src = "assets/img/Numer Box=B10.png";
+
+let wall_b11_img = new Image();
+wall_b11_img.src = "assets/img/Numer Box=B11.png";
+
+let enemy_img = new Image();
+enemy_img.src = "assets/img/enemy.png";
+
+let add_img = new Image();
+add_img.src = "assets/img/icon=add.png";
+
+let arcade_img = new Image();
+arcade_img.src = "assets/img/icon=arcade.png";
+
+let badge_img = new Image();
+badge_img.src = "assets/img/Icon=badge 2.png";
+
+let badge2_img = new Image();
+badge2_img.src = "assets/img/Icon=badge 2.png";
+
+let baul_img = new Image();
+baul_img.src = "assets/img/Icon=baul.png";
+
+let Candado_img = new Image();
+Candado_img.src = "assets/img/Icon=Candado.png";
+
+let cofre_img = new Image();
+cofre_img.src = "assets/img/Icon=cofre.png";
+
+let coin_sprite_img = new Image();
+coin_sprite_img.src = "assets/img/Item=Coin Sprite.png";
+
+let life_img = new Image();
+life_img.src = "assets/img/Item=life.png";
+
+let map_img = new Image();
+map_img.src = "assets/img/Item=Map.png";
+
+let menu_img = new Image();
+menu_omg.src = "assets/img/Item=menu.png";
+
+let set_img = new Image();
+set_img.src = "assets/img/Item=settings.png";
+
+let shield_img = new Image();
+shield_img.src = "assets/img/Item=shield.png";
+
+let star_img = new Image();
+star_img.src = "assets/img/Item=star.png";
+
+let thund_img = new Image();
+thund_img.src = "assets/img/Item=Thunder.png";
+
+let yt_img = new Image();
+yt_img.src = "assets/img/Item=youtube.png";
+
+
+function setMap(){
+    map[4].type = {name:"coin", img:coin_img};
+    map[6].type = {name:"coin", img:coin_img};
+    map[10].type = {name:"wall", img:wall_bottom1_img};
+    map[39].type = {name:"wall", img:wall_bottom1_img};
+}
